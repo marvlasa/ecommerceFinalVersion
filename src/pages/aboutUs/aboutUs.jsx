4bf2ca8559d4817ac419ac4091bf1675028cc820
marvlasa <martin.vlasa@gmail.com>
2021-05-24 10:35:18 -0300

    to uppercase About us

--- src/pages/aboutUs/aboutUs.jsx
+++ src/pages/aboutUs/aboutUs.jsx
@@ -52,12 +52,12 @@ function AboutUs() {
                 <div class="section-tittle mb-60 text-center pt-10">
                   <h2>Our Story</h2>
                   <p class="pera">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                    do eiusmod tempor incididunt ut labore et dolore magna
-                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                    ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                    Duis aute irure dolor in reprehenderit in voluptate velit
-                    esse cillum dolore eu fugiat nulla pariatur.
+                    ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                    eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                    Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                    laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+                    irure dolor in reprehenderit in voluptate velit esse cillum
+                    dolore eu fugiat nulla pariatur.
                   </p>
                 </div>
               </div>
